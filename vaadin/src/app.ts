d8833921d13a2efc1f5d50aa3443c1a1a0b0e1b1
Wiete Lueck <wiete.lueck@smail.inf.h-brs.de>
2022-07-12 21:49:20 +0200

    small fixes

---
+++ vaadin/src/app.ts
@@ -0,0 +1,21 @@
+/**
+ *  Imports fuer die Verwendung der Webkomponenten von Vaadin
+ */
+
+import '@vaadin/button';
+import '@vaadin/icon';
+import '@vaadin/icons';
+
+import '@vaadin/accordion';
+
+import '@vaadin/text-area';
+import '@vaadin/text-field';
+import '@vaadin/integer-field';
+import '@vaadin/number-field';
+
+import '@vaadin/checkbox';
+import '@vaadin/checkbox-group';
+
+import '@vaadin/radio-group';
+
+import '@vaadin/combo-box';
